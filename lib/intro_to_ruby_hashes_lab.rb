5654d50f01a86208f130ede9aeb325c80d25a701
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-29 17:58:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,19 +1,14 @@
 def my_hash_creator(key, value)
- 
   hash = {key => value}
 end
 
 def read_from_hash(hash, key)
-  
   hash[key]
 end
 
-def update_counting_hash(hash, key)
 
-  if hash[key]
-    hash[key] = hash[key] + 1
-  else
-    hash[key] = 1 
-  end
-  hash
+
+def update_counting_hash(hash, key)
+  hash[key] ? hah[key] =+1 : hash[key] = 1 
+  hash 
 end
